6bccd7051fe00c648b4fac757c83d5382cf83a05
Jusitn Gallardo <me@jirw.in>
2018-08-01 01:54:48 -0700

    Include sha256sums in releases

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,29 +6,32 @@ echo "Publishing $VERSION..."
 mkdir dist
 mkdir releases
 gox -osarch="linux/amd64" -osarch="linux/386" -osarch="darwin/amd64" -osarch="freebsd/amd64" -osarch="freebsd/386" -ldflags "-X main.Version=$VERSION" -output "dist/{{.OS}}_{{.Arch}}/osiw-server" ./cmd/osiw-server
-
-for i in dist/* ; do
-  if [ -d "$i" ]; then
-   ARCH=$(basename "$i")
-   mkdir "osiw-server_$VERSION"
-   cp "dist/$ARCH/osiw-server" "osiw-server_$VERSION"
-   zip -r "releases/osiw-server_$VERSION-$ARCH.zip" "osiw-server_$VERSION"
-   rm -rf "osiw-server_$VERSION"
-  fi
-done
-
 gox -osarch="linux/amd64" -osarch="linux/386" -osarch="darwin/amd64" -osarch="freebsd/amd64" -osarch="freebsd/386" -ldflags "-X main.Version=$VERSION" -output "dist/{{.OS}}_{{.Arch}}/osiw-client" ./cmd/osiw-client
 
 for i in dist/* ; do
   if [ -d "$i" ]; then
    ARCH=$(basename "$i")
+
+   # Server
+   mkdir "osiw-server_$VERSION"
+   cp "dist/$ARCH/osiw-server" "osiw-server_$VERSION"
+   zip -r "releases/osiw-server_$VERSION-$ARCH.zip" "osiw-server_$VERSION"
+   cd releases
+   sha256sum "osiw-server_$VERSION-$ARCH.zip" > "osiw-server_$VERSION-$ARCH.zip.sha256sum"
+   cd ..
+   rm -rf "osiw-server_$VERSION"
+
+   # Client
    mkdir "osiw-client_$VERSION"
    cp "dist/$ARCH/osiw-client" "osiw-client_$VERSION"
    zip -r "releases/osiw-client_$VERSION-$ARCH.zip" "osiw-client_$VERSION"
+   cd releases
+   sha256sum "osiw-client_$VERSION-$ARCH.zip" > "osiw-client_$VERSION-$ARCH.zip.sha256sum"
+   cd ..
    rm -rf "osiw-client_$VERSION"
   fi
 done
 
-ghr -t "$GITHUB_TOKEN" -u jirwin -r osiw --replace "$VERSION" releases/
+#ghr -t "$GITHUB_TOKEN" -u jirwin -r osiw --replace "$VERSION" releases/
 
-rm -rf dist releases
+#rm -rf dist releases
